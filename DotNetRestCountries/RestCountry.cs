0908f15b1485a3c5fd8782b1ff398147328fb122
99darshan <dsn.adh@gmail.com>
2017-12-23 21:32:30 +0545

    initial commit

---
+++ DotNetRestCountries/RestCountry.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace DotNetRestCountries
+{
+    class RestCountry
+    {
+        private static HttpClient _client = new HttpClient();
+        private const string _BASEURL = "https://restcountries.eu/rest/v2/";
+
+        // comments documentation, auto generate
+        public static async void All()
+        {
+            string requestUrl = _BASEURL + "all";
+
+            try
+            {
+                HttpResponseMessage res = await _client.GetAsync(requestUrl);
+                if (res.StatusCode == HttpStatusCode.OK)
+                {
+                    
+
+                }
+
+            }
+            catch (Exception e)
+            {
+
+            }
+
+        }
+    }
+}
