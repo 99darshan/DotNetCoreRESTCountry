9334026bff2d3c816cb3863bc096f3dc634978d9
99darshan <dsn.adh@hotmail.com>
2018-06-03 16:29:00 +0545

    added Filters.cs which contains all the fields in API as constants, this FilterConstants can be used while making a filtered request to the aPI

---
+++ DotNetRestCountries/Filters.cs
@@ -0,0 +1,28 @@
+namespace DotNetRestCountries{
+  public static class Filters{
+    public const string Name = "name";
+    public const string TopLevelDomain = "topLevelDomain";
+    public const string Alpha2Code = "alpha2code";
+    public const string Alpha3Code = "alpha3code";
+    public const string CallingCodes = "callingCodes";
+    public const string Capital = "capital";
+    public const string AltSpellings = "altSpellings";
+    public const string Region = "region";
+    public const string Subregion = "subregion";
+    public const string Population = "population";
+    public const string Latlng = "latlng";
+    public const string Demonym = "demonym";
+    public const string Area = "area";
+    public const string Gini = "gini";
+    public const string Timezones = "timezones";
+    public const string Borders = "borders";
+    public const string NativeName = "nativeName";
+    public const string NumericCode = "numericCode";
+    public const string Currencies = "currencies";
+    public const string Languages = "languages";
+    public const string Translations = "translations";
+    public const string Flag = "flag";
+    public const string RegionalBlocs = "regionalBlocs";
+    public const string Cioc = "cioc";
+  }
+}
