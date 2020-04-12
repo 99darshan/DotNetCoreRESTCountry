5376c1810d11e0168845b9110d2afd806b55a7bf
99darshan <dsn.adh@gmail.com>
2020-04-12 16:28:02 +0545

    Nuget package v1.0.3 release, updated dotnet core version to 3.1, added nuspec

--- Tests/RestCountryTests.cs
+++ Tests/RestCountryTests.cs
@@ -1,63 +1,74 @@
-using DotNetRestCountries;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using DotNetRestCountries;
 using Xunit;
 
 namespace Tests
 {
     public class RestCountryTests
     {
-        private void _NepalResultFiltered(Country con)
+        private void _NepalResultFiltered (Country con)
         {
-            Assert.NotEqual("Kathmandu", con.Capital);
-            Assert.Null(con.Currencies);
-            Assert.Null(con.RegionalBlocs);        
+            Assert.NotEqual ("Kathmandu", con.Capital);
+            Assert.Null (con.Currencies);
+            Assert.Null (con.RegionalBlocs);
         }
-        private void _NepalResultFull(Country con)
+        private void _NepalResultFull (Country con)
         {
-            Assert.Equal(28.0, con.Latlng[0]);
-            Assert.Equal(28431500, con.Population);
-            Assert.IsType<Translations>(con.Translations);
-            Assert.IsType<Language[]>(con.Languages);
-            Assert.Equal("NPR", con.Currencies[0].Code);
-            Assert.Empty(con.RegionalBlocs[0].OtherNames);
+            Assert.Equal (28.0, con.Latlng[0]);
+            Assert.Equal (28431500, con.Population);
+            Assert.IsType<Translations> (con.Translations);
+            Assert.IsType<Language[]> (con.Languages);
+            Assert.Equal ("NPR", con.Currencies[0].Code);
+            Assert.Empty (con.RegionalBlocs[0].OtherNames);
         }
+
         [Fact]
-        public void AllTest()
+        public void AllTest ()
         {
-            List<string> filters = new List<string>() { Filters.Name };
-            List<Country> allCountriesFiltered = RestCountry.All(filters);
-            List<Country> allCountries = RestCountry.All();
+            List<string> filters = new List<string> () { Filters.Name };
+            List<Country> allCountriesFiltered = RestCountry.All (filters);
+            List<Country> allCountries = RestCountry.All ();
+            List<string> filterByAlphaCode = new List<string> () { Filters.Alpha2Code };
+            List<Country> alllCountriesAlphaCodeFilter = RestCountry.All (filterByAlphaCode);
             foreach (Country con in allCountries)
             {
-                if(con.Name == "Nepal")
+                if (con.Name == "Nepal")
                 {
-                    _NepalResultFull(con);
+                    _NepalResultFull (con);
                 }
             }
 
             foreach (Country con in allCountriesFiltered)
             {
-                if(con.Name == "Nepal")
+                if (con.Name == "Nepal")
                 {
-                    _NepalResultFiltered(con);
+                    _NepalResultFiltered (con);
+                }
+            }
+
+            foreach (Country con in alllCountriesAlphaCodeFilter)
+            {
+                if (con.Name == "Nepal")
+                {
+                    Assert.Equal ("NP", con.Alpha2Code);
                 }
             }
         }
 
         [Fact]
-        public async void AllTestAsync()
+        public async void AllTestAsync ()
         {
-            List<string> filters = new List<string>() { Filters.Name };
-            List<Country> allCountries = await RestCountry.AllAsync(filters);
-            List<Country> countries = await RestCountry.AllAsync();
+            List<string> filters = new List<string> () { Filters.Name };
+            List<Country> allCountries = await RestCountry.AllAsync (filters);
+            List<Country> countries = await RestCountry.AllAsync ();
 
             foreach (Country con in allCountries)
             {
                 if (con.Name == "Nepal")
                 {
-                    _NepalResultFiltered(con);
+                    _NepalResultFiltered (con);
 
                 }
             }
@@ -66,20 +77,20 @@ namespace Tests
             {
                 if (con.Name == "Nepal")
                 {
-                    _NepalResultFull(con);
+                    _NepalResultFull (con);
                 }
             }
         }
-        
+
         // name
         [Fact]
-        public void NameTest()
+        public void NameTest ()
         {
-            List<Country> con = RestCountry.Name("ne");
-            
+            List<Country> con = RestCountry.Name ("ne");
+
             foreach (Country c in con)
             {
-                if (c.Name == "Nepal") _NepalResultFull(c);
+                if (c.Name == "Nepal") _NepalResultFull (c);
             }
 
         }
